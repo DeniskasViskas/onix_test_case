6f1f66aaa772aa9b45da675ccc5fccec221f77fa
deniskasViskas <DeniskasViskas4@gmail.com>
2021-02-12 23:39:24 +0200

    change structure

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Footer from "./Footer";
+import HeaderNav from "./HeaderNav";
+import Sidebar from "./Sidebar";
+import {ThemeStyle} from "../App";
+
 
 function Layout({children}) {
+    let theme = React.useContext(ThemeStyle)
     return (
         <>
-            <div className={"content"}>
-                <div className={"container"}>
-                    {children}
+            <div className={'wrapper '+ (theme.theme)}>
+                <Sidebar/>
+                <div className={"content"}>
+                    <HeaderNav/>
+                    <div className={"page_content"}>
+                        {children}
+                    </div>
+                    <Footer/>
                 </div>
             </div>
-            <Footer/>
         </>
     )
 }
