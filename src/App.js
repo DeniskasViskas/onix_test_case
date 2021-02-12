842feda6a28dadfd911ac29686ea04517b4e9650
deniskasViskas <DeniskasViskas4@gmail.com>
2021-02-12 23:34:28 +0200

    change structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from "react"
+import React, {useState} from "react"
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-} from 'react-router-dom'
+} from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import About from "./pages/About";
+
+const ThemeStyle = React.createContext({
+    theme: 'light',
+    setTheme: () => {}
+});
 
 function App() {
+    const [theme, setTheme] = useState('light');
     return (
+        <ThemeStyle.Provider value={{theme,setTheme}}>
         <Router>
             <Layout>
                 <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
                     <Route path="/">
-                        <Home/>
+                        <Home />
                     </Route>
                 </Switch>
             </Layout>
         </Router>
+        </ThemeStyle.Provider>
     );
 }
-export default App
+export default App;
+export {ThemeStyle}
