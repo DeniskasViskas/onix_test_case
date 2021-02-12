d6c986162cfea65351aa0a454583ed32c77c469f
deniskasViskas <DeniskasViskas4@gmail.com>
2021-02-12 23:39:56 +0200

    change component

---
+++ src/components/HeaderNav.js
@@ -0,0 +1,41 @@
+import React, {useContext} from "react";
+import {NavLink} from "react-router-dom";
+import {ThemeStyle} from "../App";
+
+function HeaderNav() {
+    const {theme,setTheme} = useContext(ThemeStyle);
+    const changeMode = ()=>{
+        console.log(theme)
+        if (theme === 'light'){
+            return setTheme('dark')
+        }else{
+            return setTheme('light')
+        }
+    }
+    return (
+        <header className={"page-header"}>
+            <ul className={"nav-list"}>
+                <li className={"nav-item"}>
+                    <NavLink activeClassName="active" exact className={'nav-link'} to="/">Home</NavLink>
+                </li>
+                <li className={"nav-item"}>
+                    <NavLink activeClassName="active" exact className={'nav-link'} to="/about">About</NavLink>
+                </li>
+            </ul>
+
+            <div className={'switch-btn text-center'}>
+                <span className={"fw-bold"}>
+                    {
+                        theme === 'light' ? 'Dark mode' : 'Light mode'
+                    }
+                </span>
+                <div className="custom-control custom-switch custom-switch-lg">
+                    <input type="checkbox" className="custom-control-input" id="theme_switch" onChange={changeMode}/>
+                    <label className="custom-control-label" htmlFor="theme_switch"/>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default HeaderNav
