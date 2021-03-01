ea4a8633c25b4097a7229c24f12ed11ff76e5391
deniskasViskas <DeniskasViskas4@gmail.com>
2021-03-01 22:49:43 +0200

    clean up

--- src/components/biography/BiographyTable.js
+++ src/components/biography/BiographyTable.js
@@ -2,38 +2,34 @@ import React from "react";
 import "./TableCSS.css"
 import TableRow from "./TableRow";
 import TableForm from "./TableForm";
-const SORT_ASC ='asc'
-const SORT_DESC= 'desc';
 
 function BiographyTable() {
     let biographyData = [
         {
             'year': 2020,
             'event': 'React tutorial',
-            'additional_info':{
-                'photo':[],
-                'person':['person1','person2']
+            'additional_info': {
+                'photo': [],
+                'person': ['person1', 'person2']
             }
         },
         {
             'year': 1992,
             'event': 'born',
-            'additional_info':{
-                'photo':[],
-                'person':['person1']
+            'additional_info': {
+                'photo': [],
+                'person': ['person1']
             }
         },
         {
             'year': 1999,
             'event': 'go to school',
-            'additional_info':{
-                'photo':[],
-                'person':['person3']
+            'additional_info': {
+                'photo': [],
+                'person': ['person3']
             }
         }
     ];
-    let sortType = '';
-    let sortProp = '';
 
     function getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
@@ -41,81 +37,48 @@ function BiographyTable() {
         return Math.floor(Math.random() * (max - min + 1)) + min //Максимум и минимум включаются
     }
 
-    function sortByProp(prop) {
-        return function (a, b) {
-            if (a[prop] > b[prop]) {
-                return 1;
-            } else if (a[prop] < b[prop]) {
-                return -1;
-            }
-            return 0;
-        }
-    }
-
-    function sortData(prop, type) {
-        sortProp = prop
-        biographyData.sort(sortByProp(prop));
-        if (type === SORT_DESC) {
-            biographyData.reverse()
-        }
-        sortType = (sortType === SORT_DESC) ? SORT_ASC : SORT_DESC
-        console.log(biographyData)
-    }
-
     function add(data, action) {
+        console.log(biographyData,'start add')
         switch (action) {
+            default:
             case 'push':
                 // biographyData = biographyData.concat(data)
-                biographyData = [...biographyData,{...data}]
+                biographyData = [...biographyData, {...data}]
                 break
             case 'unshift':
                 // biographyData = [data].concat(biographyData)
-                biographyData = [{...data},...biographyData]
+                biographyData = [{...data}, ...biographyData]
                 break
             case 'index':
                 let index = getRandomIntInclusive(0, biographyData.length);
                 biographyData.splice(index, 0, {...data})
                 break
         }
-        console.log(biographyData)
+        console.log(biographyData,'end add')
     }
 
     function remove(index) {
-        console.log(
-            biographyData.filter((item, i) => {
-                return i !== index
-            })
-        )
+        console.log(biographyData,'start delete')
+        let arr = biographyData.filter((item, i) => {
+            return i !== index
+        })
+        console.log(arr,'end delete')
     }
 
     function bubbleSort(prop) {
-        let n = biographyData.length;
+        let arr = [...biographyData];
+        let n = arr.length;
+        console.log(arr,'start bubbleSort')
         for (let i = 0; i < n - 1; i++) {
             for (let j = 0; j < n - 1 - i; j++) {
-                if (biographyData[j + 1][prop] < biographyData[j][prop]) {
-                    let t = biographyData[j + 1][prop]
-                    biographyData[j + 1][prop] = biographyData[j][prop]
-                    biographyData[j][prop] = t
+                if (+arr[j + 1][prop] < +arr[j][prop]) {
+                    let t = arr[j + 1]
+                    arr[j + 1] = arr[j]
+                    arr[j] = t
                 }
             }
         }
-        console.log(biographyData)
-        return biographyData;
-    }
-
-    function selectionSort(prop){
-        let n = biographyData.length
-        for (let i = 0; i < n - 1; i++) {
-            let min = i
-            for (let j = i + 1; j < n; j++) {
-                if (biographyData[j][prop] < biographyData[min][prop]) min = j
-            }
-            let t = biographyData[min][prop]
-            biographyData[min][prop] = biographyData[i][prop]
-            biographyData[i][prop] = t
-        }
-        console.log(biographyData)
-        return biographyData;
+        console.log(arr,'end bubbleSort')
     }
 
     return (
@@ -138,26 +101,10 @@ function BiographyTable() {
                         <tr className={"table-row"}>
                             <th className={"table-cell"}><span>id</span></th>
                             <th className={"table-cell cell-sortable"}>
-                                <span onClick={() => sortData('year', sortType)}>year</span>
-                                {
-                                    sortProp === 'year' && sortType === SORT_ASC &&
-                                    <i className="fas fa-long-arrow-alt-up"/>
-                                }
-                                {
-                                    sortProp === 'year' && sortType === SORT_DESC &&
-                                    <i className="fas fa-long-arrow-alt-down"/>
-                                }
+                                <span>year</span>
                             </th>
                             <th className={"table-cell cell-sortable"}>
-                                <span onClick={() => sortData('event', sortType)}>event</span>
-                                {
-                                    sortProp === 'event' && sortType === SORT_ASC &&
-                                    <i className="fas fa-long-arrow-alt-up"/>
-                                }
-                                {
-                                    sortProp === 'event' && sortType === SORT_DESC &&
-                                    <i className="fas fa-long-arrow-alt-down"/>
-                                }
+                                <span>event</span>
                             </th>
                             <th className={"table-cell"}>
                                 <span>Person</span>
@@ -176,11 +123,10 @@ function BiographyTable() {
                         </tbody>
                     </table>
                     <div className={"btn-group"}>
-                        <div className={"btn btn-danger"} onClick={() => {bubbleSort('year')}}>
+                        <div className={"btn btn-danger"} onClick={() => {
+                            bubbleSort('year')
+                        }}>
                             Bubble Sort
-                        </div>
-                        <div className={"btn btn-success"} onClick={() => {selectionSort('year')}}>
-                            Selection Sort
                         </div>
                     </div>
                 </div>
