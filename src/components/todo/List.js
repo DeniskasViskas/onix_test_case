ce8373abefadeb88b6e849fdf00b72218fa5a834
deniskasViskas <DeniskasViskas4@gmail.com>
2021-03-11 07:32:42 +0200

    dispatch props

--- src/components/todo/List.js
+++ src/components/todo/List.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 class List extends React.Component{
+    constructor({title,count,isActive,changeList}) {
+        super({title,count,isActive,changeList});
+    }
     render() {
         return(
-            <div className={'navi-item'} onClick={()=>this.props.changeList(this.props.title)}>
-                <div className={'navi-link'+(this.props.isActive ? ' active':'')}>
+            <div className={'navi-item'} onClick={()=>this.changeList(this.title)}>
+                <div className={'navi-link'+(this.isActive ? ' active':'')}>
                     <span className={'navi-icon'}><i className="fas fa-chevron-right"/></span>
-                    <span className={'navi-text text-capitalize'}>{this.props.title}</span>
+                    <span className={'navi-text text-capitalize'}>{this.title}</span>
                     <span className={'navi-label'}>
-                                    <span className={'badge badge-primary'}>{this.props.count}</span>
+                                    <span className={'badge badge-primary'}>{this.count}</span>
                                 </span>
                 </div>
             </div>
