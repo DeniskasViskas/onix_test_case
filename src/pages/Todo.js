370da6f9e8a3549016d7dfbb13151e5b9744dea9
Denis Diachenko <deniskasViskas4@gmail.com>
2021-04-04 19:29:13 +0300

    add Custom DnD

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -4,6 +4,7 @@ import TodoList from "../components/todo/TodoList";
 import TodoTasks from "../components/todo/TotoTasks";
 import {DragDropContext} from "react-beautiful-dnd";
 import {Alert} from "react-bootstrap";
+import CustomDnD from "../components/todo/CustomDnD";
 
 const todos = [];
 const categories = []
@@ -11,11 +12,6 @@ const categories = []
 class Todo extends React.Component {
     constructor(props) {
         super(props);
-        this.markTack = this.markTack.bind(this)
-        this.createCategory = this.createCategory.bind(this)
-        this.createTack = this.createTack.bind(this)
-        this.getCategoryTask = this.getCategoryTask.bind(this)
-        this.getCategory = this.getCategory.bind(this)
         this.check_img_url = '';
         this.state = {
             todos: todos,
@@ -37,7 +33,7 @@ class Todo extends React.Component {
         }
     }
 
-    getCategory() {
+    getCategory = ()=> {
         fetch('http://localhost:3001/todo/category')
             .then((response) => {
                 return response.json()
@@ -47,7 +43,7 @@ class Todo extends React.Component {
             })
     }
 
-    getCategoryTask(cat = '') {
+    getCategoryTask = (cat = '')=> {
         fetch('http://localhost:3001/todo/tasks/' + cat, {
             headers: {
                 'Content-Type': 'application/json',
@@ -62,7 +58,7 @@ class Todo extends React.Component {
             })
     }
 
-    createTack(task) {
+    createTack = (task)=> {
         fetch('http://localhost:3001/todo/tasks', {
             method: 'POST',
             headers: {
@@ -80,7 +76,7 @@ class Todo extends React.Component {
             })
     }
 
-    createCategory(name) {
+    createCategory = (name) => {
         if (name && name !== '') {
             fetch('http://localhost:3001/todo/category', {
                 method: 'POST',
@@ -99,7 +95,7 @@ class Todo extends React.Component {
         }
     }
 
-    markTack(id, prop) {
+    markTack = (id, prop) => {
         fetch('http://localhost:3001/todo/tasks/' + id, {
             method: 'PUT',
             headers: {
@@ -116,13 +112,11 @@ class Todo extends React.Component {
             })
     }
 
-    DragHandler = event => {
+    DragHandler = (event) => {
         const {destination, source} = event;
         if (destination) {
             let from = source.index;
             let to = destination.index;
-            // let item = this.state.todos.splice(from,1)
-            // this.state.todos.splice(to,0,item[0])
             this.state.todos.map((item) => {
                 if (item.index === from) {
                     return item.index = to
@@ -139,70 +133,72 @@ class Todo extends React.Component {
     }
 
     render() {
+        const {categories,activeCategory,todos,img_load_status,check_img_url} = this.state
         return (
             <div className={'container-fluid'}>
                 <div className={'d-flex flex-row'}>
                     <div className={'flex-row-auto'}>
                         <TodoList
-                            categories={this.state.categories}
+                            categories={categories}
                             changeList={(val) => this.setState({activeCategory: val})}
-                            activeCategory={this.state.activeCategory}
+                            activeCategory={activeCategory}
                             createCategory={this.createCategory}
                         />
                     </div>
                     <div className={'flex-row-fluid ml-6'}>
                         <DragDropContext onDragEnd={this.DragHandler}>
                             <TodoTasks
-                                activeCategory={this.state.activeCategory}
-                                todos={this.state.todos}
+                                activeCategory={activeCategory}
+                                todos={todos}
                                 markTack={this.markTack}
                                 createTack={this.createTack}
                             />
                         </DragDropContext>
                     </div>
                 </div>
-                <div className={'flex-row'}>
-                    <div className={'flex-row-auto'}>
-                        <div className="flex-row-fluid">
-                            <div className={'card custom-card mb-6 mt-6 col-lg-5 col-12'}>
-                                <div className="card-header bg-white">
-                                    <div className="card-title">
-                                        <h3 className={'text-uppercase'}>Check Image</h3>
-                                        <div className={"text-muted"}>by Url</div>
-                                    </div>
+                <div className={'row mt-6'}>
+                    <div className="col-12 col-lg-6">
+                        <div className={'card custom-card'}>
+                            <div className="card-header bg-white">
+                                <div className="card-title">
+                                    <h3 className={'text-uppercase'}>Check Image</h3>
+                                    <div className={"text-muted"}>by Url</div>
                                 </div>
-                                <div className={'card-body text-center'}>
-                                    <form action="">
-                                        <div className="input-group mb-3">
-                                            <input type="text" name={'url'} className="form-control" required
-                                                   placeholder="type url"
-                                                   onChange={event => this.check_img_url  = event.target.value}
-                                            />
-                                            <div className="input-group-append">
-                                                <button className="btn btn-primary" type={"submit"} onClick={(event)=>{
-                                                    event.preventDefault()
-                                                    this.setState({check_img_url:this.check_img_url})
-                                                }} >Chek</button>
-                                            </div>
+                            </div>
+                            <div className={'card-body text-center'}>
+                                <form action="">
+                                    <div className="input-group mb-3">
+                                        <input type="text" name={'url'} className="form-control" required
+                                               placeholder="type url"
+                                               onChange={event => this.check_img_url  = event.target.value}
+                                        />
+                                        <div className="input-group-append">
+                                            <button className="btn btn-primary" type={"submit"} onClick={(event)=>{
+                                                event.preventDefault()
+                                                this.setState({check_img_url:this.check_img_url})
+                                            }} >Chek</button>
                                         </div>
-                                    </form>
-                                    <div>
-                                        {
-                                            this.state.check_img_url.length > 1  &&
-                                            // eslint-disable-next-line jsx-a11y/img-redundant-alt
-                                                <img src={this.state.check_img_url} alt={"chek image"}
-                                                     width={100} height={100}
-                                                     onError={()=>this.setState({img_load_status:false})}
-                                                     onLoad={()=>this.setState({img_load_status:true})}
-                                                />
-                                        }
-                                        <Alert variant={'danger'} show={!this.state.img_load_status}>
-                                            Url <b>{this.state.check_img_url}</b>  not <b>available</b>
-                                        </Alert>
                                     </div>
+                                </form>
+                                <div>
+                                    {
+                                        check_img_url.length > 1  &&
+                                        // eslint-disable-next-line jsx-a11y/img-redundant-alt
+                                            <img src={check_img_url} alt={"chek image"}
+                                                 width={100} height={100}
+                                                 onError={()=>this.setState({img_load_status:false})}
+                                                 onLoad={()=>this.setState({img_load_status:true})}
+                                            />
+                                    }
+                                    <Alert variant={'danger'} show={!img_load_status}>
+                                        Url <b>{check_img_url}</b>  not <b>available</b>
+                                    </Alert>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        <CustomDnD/>
                     </div>
                 </div>
             </div>
